90bb78a30a5b941c926027eaaf804f30003f89ea
Jakub Jirutka <jakub@jirutka.cz>
2016-12-10 01:54:12 +0100

    Fix build to really disable stage3 and stage4

--- build.sh
+++ build.sh
@@ -8,7 +8,11 @@ export LC_ALL='C.UTF-8'
 cd "$(dirname "$0")"
 
 mkdir -p build
-cp -rf pi-gen/* pi-gen-overlay/* build/
-
 cd build
+
+# Assembly our customized pi-gen.
+cp -rf ../pi-gen/* ../pi-gen-overlay/* .
+rm -rf stage2/EXPORT_NOOBS stage3 stage4
+
+# Build customized stage2 image.
 ./build.sh
