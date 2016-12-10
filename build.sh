029e0f89e8ecd98e42a3ea62cecf532bc0fdf663
Jakub Jirutka <jakub@jirutka.cz>
2016-12-10 02:46:03 +0100

    Change image/zip file name to bc-hub-$GIT_TAG-armhf-rpi.img

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@ export IMG_NAME='bc-hub'
 export DEBIAN_FRONTEND='noninteractive'
 export LC_ALL='C.UTF-8'
 
+if [ -n "${TRAVIS_TAG:-}" ]; then
+	export IMG_DATE="$TRAVIS_TAG"
+fi
+
 cd "$(dirname "$0")"
 
 mkdir -p build
@@ -13,6 +17,11 @@ cd build
 # Assembly our customized pi-gen.
 cp -rf ../pi-gen/* ../pi-gen-overlay/* .
 rm -rf stage2/EXPORT_NOOBS stage3 stage4
+
+for file in ../pi-gen-overlay/*.patch; do
+	echo "Applying patch ${file%/*}"
+	patch -p1 < "$file"
+done
 
 # Build customized stage2 image.
 ./build.sh
