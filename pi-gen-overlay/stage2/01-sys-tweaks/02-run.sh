6718bf777b8578e10da6804a5a6e9ba7a4873c4c
Jakub Jirutka <jakub.jirutka@bigclown.com>
2017-01-20 22:03:12 +0100

    Really change timezone to Europe/Prague

--- pi-gen-overlay/stage2/01-sys-tweaks/02-run.sh
+++ pi-gen-overlay/stage2/01-sys-tweaks/02-run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -e
 
 # Apply timezone changes.
+# Note: Change via debconf doesn't work, so we do it directly and use
+# dpkg-reconfigure just to update /etc/localtime.
 on_chroot <<-EOF
+	echo 'Europe/Prague' > /etc/timezone
 	dpkg-reconfigure -f noninteractive tzdata
 EOF
