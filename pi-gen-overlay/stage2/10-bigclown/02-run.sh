2e4f5d0e1466c535d41a372cc1859ddcd9e2adf0
Karel Blavka <karel.blavka@bigclown.com>
2017-07-25 13:34:30 +0200

    Fix install node-red

--- pi-gen-overlay/stage2/10-bigclown/02-run.sh
+++ pi-gen-overlay/stage2/10-bigclown/02-run.sh
@@ -1,16 +1,17 @@
 #!/bin/bash -e
 
-echo "Install node-red"
+install -m 644 nodered.service "$ROOTFS_DIR"/etc/systemd/system/
 
+on_chroot << EOF
 npm i -g --unsafe-perm --no-progress node-red
 npm i -g --unsafe-perm --no-progress node-red-contrib-blynk-websockets
 npm i -g --unsafe-perm --no-progress node-red-node-twitter
 npm i -g --unsafe-perm --no-progress node-red-dashboard
 npm i -g --unsafe-perm --no-progress node-red-contrib-counter
 
-install -m 644 nodered.service "$ROOTFS_DIR"/etc/systemd/system/
-
-on_chroot << EOF
 systemctl enable nodered.service
 systemctl start nodered.service
 EOF
+
+
+
