8da6e3eff16e733183c5fa1c488cdf4457b3aa9f
Karel Blavka <karel.blavka@bigclown.com>
2017-07-25 12:46:22 +0200

    Add node red

---
+++ pi-gen-overlay/stage2/10-bigclown/02-run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+install -m 644 files/nodered.service "$ROOTFS_DIR"/etc/systemd/system/
+
+on_chroot << EOF
+npm i -g --unsafe-perm --no-progress node-red
+npm i -g --unsafe-perm --no-progress node-red-contrib-blynk-websockets
+npm i -g --unsafe-perm --no-progress node-red-node-twitter
+npm i -g --unsafe-perm --no-progress node-red-dashboard
+npm i -g --unsafe-perm --no-progress node-red-contrib-counter
+
+systemctl enable nodered.service
+systemctl start nodered.service
+EOF
+
+
+
