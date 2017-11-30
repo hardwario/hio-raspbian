52b3707f6c3efd17638b295b67c6a64469da3170
Karel Blavka <karel.blavka@bigclown.com>
2017-11-30 07:32:00 +0100

    Add udev rules

--- pi-gen-overlay/stage2/10-bigclown/00-run.sh
+++ pi-gen-overlay/stage2/10-bigclown/00-run.sh
@@ -7,6 +7,8 @@ for file in files/*.gpg.key; do
 	on_chroot apt-key add - < "$file"
 done
 
+install -m 644 files/*.rules "$ROOTFS_DIR"/etc/udev/rules.d/
+
 install -m 444 files/libssl1.0.0_1.0.1t-1+deb8u7_armhf.deb "$ROOTFS_DIR/tmp"
 install -m 444 files/libwebsockets3_1.2.2-1_armhf.deb "$ROOTFS_DIR/tmp"
 
