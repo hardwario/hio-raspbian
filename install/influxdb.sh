b521c35076ffe62a5c3c8acceb62199172fc94b3
Pavel Hübner <pavel.hubner@bigclown.com>
2018-01-07 10:09:45 +0100

    Fix typo

--- install/influxdb.sh
+++ install/influxdb.sh
@@ -8,7 +8,7 @@ step() {
 	printf '\n\033[1;36m%d) %s\033[0m\n' $_step_counter "$@" >&2  # bold cyan
 }
 
-step 'Upgrade and Update'
+step 'Update and upgrade packages'
 sudo apt update && sudo apt upgrade -y
 
 step 'Install dependencies'
@@ -17,13 +17,13 @@ sudo apt install apt-transport-https curl -y
 step 'Add repository key'
 curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
 
-step 'Add source list'
+step 'Add repository to source list'
 echo "deb https://repos.influxdata.com/debian jessie stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
 
-step 'Install'
+step 'Install packages'
 sudo apt update && sudo apt install influxdb
 
-step 'Eneble service'
+step 'Enable service'
 sudo systemctl daemon-reload
 sudo systemctl enable influxdb
 
