5352ada1ecc609ba683d4687f45e0fc3fd72897a
Karel Blavka <karel.blavka@bigclown.com>
2018-06-02 14:15:22 +0200

    Add update npm, Add install bch, Add install node-red-dashboard

--- pi-gen-overlay/stage2/10-bigclown/02-run.sh
+++ pi-gen-overlay/stage2/10-bigclown/02-run.sh
@@ -12,6 +12,11 @@ install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/pi/.pm2"
 install -v -o 1000 -g 1000 files/dump.pm2 "$ROOTFS_DIR/home/pi/.pm2/dump.pm2"
 install -m 444 files/pm2-pi.service "$ROOTFS_DIR/etc/systemd/system/pm2-pi.service"
 
+# Update npm
+on_chroot << EOF
+npm install -g  --unsafe-perm npm@latest
+EOF
+
 on_chroot << EOF
 npm install -g --unsafe-perm --no-progress pm2
 
@@ -22,6 +27,10 @@ EOF
 # Install Node-RED
 on_chroot << EOF
 npm install -g --unsafe-perm --no-progres node-red
+
+cd /usr/lib/
+
+npm install -g --unsafe-perm node-red-dashboard
 EOF
 
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/pi/.node-red"
@@ -29,17 +38,23 @@ install -v -o 1000 -g 1000 files/flows_hub.json "$ROOTFS_DIR/home/pi/.node-red/f
 
 # Update pip3
 on_chroot << EOF
-pip3 install --upgrade --no-cache-dir pip
+pip3 --version
+pip3 install --upgrade pip
 EOF
 
 # Install BigClown Firmware Tool
 on_chroot << EOF
-pip3 install --upgrade --no-cache-dir bcf
+pip3 install --upgrade bcf
 EOF
 
 # Install BigClown Gateway
 on_chroot << EOF
-pip3 install --upgrade --no-cache-dir bcg
+pip3 install --upgrade bcg
+EOF
+
+# Install BigClown Control tool
+on_chroot << EOF
+pip3 install --upgrade bch
 EOF
 
 install -m 644 files/bash_completion.d/* "$ROOTFS_DIR"/etc/bash_completion.d/
