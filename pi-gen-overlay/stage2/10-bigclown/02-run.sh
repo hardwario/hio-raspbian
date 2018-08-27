03137d53ab367d5368fc75f8f8c720ba955ab6df
Karel Blavka <karel.blavka@bigclown.com>
2018-08-27 08:53:00 +0200

    Create folder /var/www

--- pi-gen-overlay/stage2/10-bigclown/02-run.sh
+++ pi-gen-overlay/stage2/10-bigclown/02-run.sh
@@ -38,6 +38,8 @@ on_chroot << EOF
 npm install -g --unsafe-perm http-server
 EOF
 
+install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/var/www"
+
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/pi/.node-red"
 install -v -o 1000 -g 1000 files/flows_hub.json "$ROOTFS_DIR/home/pi/.node-red/flows_hub.json"
 
