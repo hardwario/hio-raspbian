bd1a2429d5005fca1c1b88ea121d752b11ecc5e5
Karel Blavka <karel.blavka@bigclown.com>
2018-10-01 10:55:58 +0200

    Update mosquitto config

--- pi-gen-overlay/stage2/10-bigclown/02-run.sh
+++ pi-gen-overlay/stage2/10-bigclown/02-run.sh
@@ -78,3 +78,4 @@ EOF
 install -m 644 files/bash_completion.d/* "$ROOTFS_DIR"/etc/bash_completion.d/
 install -m 755 files/update-motd.d/* "$ROOTFS_DIR"/etc/update-motd.d/
 install -m 755 files/websocket.conf "$ROOTFS_DIR"/etc/mosquitto/conf.d/websocket.conf
+install -m 755 files/mqtt.conf "$ROOTFS_DIR"/etc/mosquitto/conf.d/mqtt.conf
